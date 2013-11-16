8b84568391f07ef0808b4a0578fc131ab339fd8a
Erdal Karan <>
2013-11-16 11:07:28 +0200

    personel sinifi eklendi

---
+++ lib/Personel.php
@@ -0,0 +1,14 @@
+<?php
+
+class Personel extends Kisi {
+    
+    public $birim;
+    public $sicilNo;
+    
+    public function getGorevYili() {
+        return date('Y') - $this->baslangicYili;
+        
+    }
+
+}
+
