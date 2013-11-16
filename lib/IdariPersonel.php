31654ec166af6ebe8e8cb5e90aa9662c0fc4a55b
Erdal Karan <>
2013-11-16 11:22:53 +0200

    personel sinifi eklendi

---
+++ lib/IdariPersonel.php
@@ -0,0 +1,19 @@
+<?php
+
+class IdariPersonel extends Personel {
+    public $gorevi;
+    
+    public function getAltPersoneller() {
+        $personeller=array (
+            'Orhan Gencebay',
+            'Ferdi Tayfur',
+            'Cengiz Kurtoğlu',
+            'Bülent Serttaç',
+            'Mahsun Kırmızıgül',
+            'Müslüm Gürses',
+            'Bülent Ersoy',
+        );
+       
+        
+    }
+}
